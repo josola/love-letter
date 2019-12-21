12c2845dc88031b49f823a31d66a148dc9296568
Jordan Sola <jordan.sola@gmail.com>
2019-12-21 09:05:05 -0600

    Add member functions to Suitor class: - Check if card is in hand vector cardInHand() - Insert item integer within hand vector insertWithinHand()

--- SuitorClass.h
+++ SuitorClass.h
@@ -5,21 +5,48 @@
 class Suitor
 {
 private:
+
 	bool
 		hasSpy = false,
 		hasHandmaid = false,
 		isPlaying = true;
+
 	int tokenCount = 0;
 
+	std::vector<int> hand;
+
 public:
+
 	void GainSpy() { hasSpy = true; }
 	void RemoveSpy() { hasSpy = false; }
-	bool SpyStatus() { return hasSpy; }
 	void GainHandmaid() { hasHandmaid = true; }
 	void RemoveHandmaid() { hasHandmaid = false; }
-	bool HandmaidStatus() { return hasHandmaid; }
-	int GetTokenCount() { return tokenCount; }
 	void GainToken() { tokenCount++; }
+	//Needs to replace current dealing logic
+	int insertWithinHand(int itemNum)
+	{
+		hand.push_back(itemNum);
+	}
+
+	int GetTokenCount() { return tokenCount; }
+	
+	bool HandmaidStatus() { return hasHandmaid; }
+	bool SpyStatus() { return hasSpy; }
+	//Integration waiting on dealing logic update
+	bool cardInHand(int cardNum) const
+	{
+		for (int i : hand)
+		{
+			if (i == cardNum)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
 };
 
 Suitor
