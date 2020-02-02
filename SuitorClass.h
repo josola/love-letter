6a4af4dbe337a5f30e245d25048c17e1ba12d468
Jordan Sola <jordan.sola@gmail.com>
2020-02-02 15:22:03 -0600

    class structure update Update position of public members and private members to reflect cpp style guidline.

--- SuitorClass.h
+++ SuitorClass.h
@@ -28,27 +28,22 @@ SOFTWARE.
 
 class Suitor
 {
-private:
-
-	bool hasSpy = false;
-	bool hasHandmaid = false;
-
-	int tokenCount = 0;
-
 public:
-
 	void GainSpy() { hasSpy = true; }
 	void RemoveSpy() { hasSpy = false; }
 	void GainHandmaid() { hasHandmaid = true; }
 	void RemoveHandmaid() { hasHandmaid = false; }
 	void GainToken() { tokenCount++; }
-
 	int GetTokenCount() { return tokenCount; }
-	
 	bool HandmaidStatus() { return hasHandmaid; }
 	bool SpyStatus() { return hasSpy; }
+private:
+	bool hasSpy = false;
+	bool hasHandmaid = false;
+	int tokenCount = 0;
 };
 
 Suitor suitor1, suitor2, suitor3, suitor4, suitor5, suitor6;
 
-std::vector<Suitor> suitorObjects { suitor1, suitor2, suitor3, suitor4, suitor5, suitor6 };
+std::vector<Suitor> suitorObjects
+	{ suitor1, suitor2, suitor3, suitor4, suitor5, suitor6 };
