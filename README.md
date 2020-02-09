7b5ba23d317dbb21ba1527e0fe2c062254382629
Jordan Sola <jordan.sola@gmail.com>
2020-02-08 22:25:47 -0600

    folder structure

--- README.md
+++
@@ -1,5 +0,0 @@
-### 💖 Interactive Love Letter (ILL)
-
-Welcome to the **INTERACTIVE LOVE LETTER (ILL)** repository. This repository contains code written to run a console app resembling the popular card game LOVE LETTER 💌, but written as if it were an interactive fiction *(text adventure)* 🎮. The author's intent for this repository is research into the respository's language.
-
-This is an _**UNOFFICIAL**_ command-line version of the popular card game LOVE LETTER 💌. the Love Letter copyright is owned and maintained by Z-Man Games. This project is a fan project dedicated to the game and is not intended to be distributed for profit. 💰
---
+++ include/global_resources.h
@@ -0,0 +1,87 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jordan Sola
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+int active_player_count;
+int original_player_count;
+int current_player;
+int token_count_to_win;
+int card_num;
+int round_count = 1;
+int temp_victor;
+int iLength;
+int car_position;
+int human_player;
+
+const int spy = 0;
+const int guard = 1;
+const int priest = 2;
+const int baron = 3;
+const int handmaid = 4;
+const int prince = 5;
+const int chancellor = 6;
+const int king = 7;
+const int countess = 8;
+const int princess = 9;
+const int starting_round = 1;
+const int min_players = 2;
+const int max_players = 6;
+
+vector<int> temp_vector;
+vector<int> players;
+vector<int> up_pile;
+vector<int> down_pile;
+vector<int> temp_input;
+vector<int> players_with_handmaid;
+
+vector<vector<int> > active_player_hands;
+
+auto winner = 0;
+unsigned int target_hum = 0;
+bool game_over = false;
+char input;
+
+const vector<string> player_names
+	{ "SUITOR 1", "SUITOR 2", "SUITOR 3", "SUITOR 4", "SUITOR 5", "SUITOR 6" };
+
+const vector<string> card_names
+	{ "SPY[0]","GUARD[1]","PRIEST[2]","BARON[3]","HANDMAID[4]","PRINCE[5]"
+	"CHANCELLOR[6]","KING[7]","COUNTESS[8]","PRINCESS[9]" };
+
+vector<int> base_deck
+	{ spy, spy, guard, guard, guard, guard, guard, guard, priest, priest,
+	baron, baron, handmaid, handmaid, prince, prince, chancellor,
+	chancellor, king, countess, princess };
+
+vector<int> playing_deck
+	{ spy, spy, guard, guard, guard, guard, guard, guard, priest, priest,
+	baron, baron, handmaid, handmaid, prince, prince, chancellor,
+	chancellor, king, countess, princess };
