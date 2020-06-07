315970e10d56a7f90cc1131d7d30d70513fd9768
josola <jordan.sola@gmail.com>
2020-06-07 12:16:42 -0500

    Update file containing functions.

--- src/Functions.h
+++ src/Functions.h
@@ -1,4 +1,5 @@
 /*
+ * Functions
  * Functions that determine gameplay capabilities.
  * Written by Jordan Sola 2019 2020
  * This software uses the MIT license.
@@ -7,30 +8,27 @@
 #ifndef FUNCTIONS_h
 #define FUNCTIONS_h
 
-//Game functions.
-void
-PrintUpPile(),
-PrintDeckSize(),
-PrintSuitorsWithSpy(),
-SwitchSuitor(),
-DiscardPlayedCard(),
-PrintActiveSuitors(),
-ChooseTargetSuitor(int target),
-PlayCard(),
-InitialSetup(),
-SuitorTurn(),
-EndRound(),
-PlayCard(),
-ClearScreen(),
-PlayGame(),
-PrintSeperator(),
-ClearInput(),
-RemoveSuitor(int suitor),
-SetWinningTokenCount(),
-ShuffleDeck(),
-ResetDeck(),
-BeginRound();
-
+void PrintUpPile();
+void PrintDeckSize();
+void PrintSuitorsWithSpy();
+void SwitchSuitor();
+void DiscardPlayedCard();
+void PrintActiveSuitors();
+void ChooseTargetSuitor(int target);
+void PlayCard();
+void InitialSetup();
+void SuitorTurn();
+void EndRound();
+void PlayCard();
+void ClearScreen();
+void PlayGame();
+void PrintSeperator();
+void ClearInput();
+void RemoveSuitor(int suitor);
+void SetWinningTokenCount();
+void ShuffleDeck();
+void ResetDeck();
+void BeginRound();
 void returnSuitor(int suitor);
 void ClearScreen();
 void PrintSeperator();
@@ -39,30 +37,24 @@ void PrintDeckSize();
 void PrintSuitorsWithSpy();
 void PrintActiveSuitors();
 void printHand(int suitor);
-
-bool
-TargetHandmaidProtected(),
-ProperCardInput(),
-CountessRestriction(),
-IsSuitorPlaying(),
-ProperSuitorInput(),
-CardInHand(int suitor, int card),
-ProperCardInput(),
-ProperSuitorInput(),
-ProperSuitorCount();
-
-//Card functions.
-void
-Spy(),
-Guard(),
-Priest(),
-Baron(),
-Handmaid(),
-Prince(),
-Chancellor(),
-King(),
-Princess();
-
+bool TargetHandmaidProtected();
+bool ProperCardInput();
+bool CountessRestriction();
+bool IsSuitorPlaying();
+bool ProperSuitorInput();
+bool CardInHand(int suitor, int card);
+bool ProperCardInput();
+bool ProperSuitorInput();
+bool ProperSuitorCount();
+void Spy();
+void Guard();
+void Priest();
+void Baron();
+void Handmaid();
+void Prince();
+void Chancellor();
+void King();
+void Princess();
 int HandPosition(int suitor, int pos);
 
 #endif
