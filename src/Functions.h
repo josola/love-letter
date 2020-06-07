c53eef23fa681d0aae2d0a8f82759ef7d9bff618
josola <jordan.sola@gmail.com>
2020-06-07 12:24:18 -0500

    Preplanning for structure update in function file.

--- src/Functions.h
+++ src/Functions.h
@@ -8,44 +8,49 @@
 #ifndef FUNCTIONS_h
 #define FUNCTIONS_h
 
+//to go into an output header
 void PrintUpPile();
 void PrintDeckSize();
 void PrintSuitorsWithSpy();
+void PrintActiveSuitors();
+void ClearScreen();
+void PrintSeperator();
+void returnSuitor(int suitor);
+void printHand(int suitor);
+
+//to go into an input header
+void ClearInput();
+bool ProperCardInput();
+bool ProperSuitorInput();
+bool ProperCardInput();
+bool ProperSuitorCount();
+
+//to go into a suitor header
 void SwitchSuitor();
 void DiscardPlayedCard();
-void PrintActiveSuitors();
 void ChooseTargetSuitor(int target);
 void PlayCard();
+void RemoveSuitor(int suitor);
+bool TargetHandmaidProtected();
+bool CountessRestriction();
+bool IsSuitorPlaying();
+bool CardInHand(int suitor, int card);
+int HandPosition(int suitor, int pos);
+
+//to go into a game state header
 void InitialSetup();
 void SuitorTurn();
 void EndRound();
 void PlayCard();
-void ClearScreen();
 void PlayGame();
-void PrintSeperator();
-void ClearInput();
-void RemoveSuitor(int suitor);
 void SetWinningTokenCount();
+void BeginRound();
+
+//to go into a deck header
 void ShuffleDeck();
 void ResetDeck();
-void BeginRound();
-void returnSuitor(int suitor);
-void ClearScreen();
-void PrintSeperator();
-void PrintUpPile();
-void PrintDeckSize();
-void PrintSuitorsWithSpy();
-void PrintActiveSuitors();
-void printHand(int suitor);
-bool TargetHandmaidProtected();
-bool ProperCardInput();
-bool CountessRestriction();
-bool IsSuitorPlaying();
-bool ProperSuitorInput();
-bool CardInHand(int suitor, int card);
-bool ProperCardInput();
-bool ProperSuitorInput();
-bool ProperSuitorCount();
+
+//to go into a card header
 void Spy();
 void Guard();
 void Priest();
@@ -55,6 +60,5 @@ void Prince();
 void Chancellor();
 void King();
 void Princess();
-int HandPosition(int suitor, int pos);
 
 #endif
