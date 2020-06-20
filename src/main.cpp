391c39fb0ed808817e19d4ff98afd1f7551be0f7
josola <jordan.sola@gmail.com>
2020-06-20 14:34:35 -0500

    Main dev update.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,28 @@
  */
 
 #include "card.h"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main()
 {
+    cout << "Enter number of players: ";
+    int player_count = 0;
+    while (player_count < 2 || player_count > 6)
+    {
+        cin >> player_count;
+        if (player_count < 2 || player_count > 6)
+        {
+            cout << "Invalid input!" << endl;
+            cin.clear();
+        }
+        else
+        {
+            break;
+        }
+    }
     return 0;
 }
