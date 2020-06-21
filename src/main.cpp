c00c7ec174f5577c6857dc321998095cee33734b
Jordan Sola <jordan.sola@gmail.com>
2020-06-21 10:17:24 -0500

    Working update!

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
-/*
- * main.cpp
- * Written by Jordan Sola
- * (C) Jordan Sola 2020 - MIT License
- */
-
-#include "card.h"
+#include "Functions.h"
 
 int main()
 {
-    return 0;
+	PlayGame();
+	return 0;
 }
