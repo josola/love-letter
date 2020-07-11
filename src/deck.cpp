afbef633f348476f2178fab131e7921cda0b73f4
josola <jordan.sola@gmail.com>
2020-07-10 22:46:09 -0500

    Update to source formatting for Deck class.

--- src/deck.cpp
+++ src/deck.cpp
@@ -25,33 +25,19 @@ void Deck::Clear() { pile_.erase(pile_.begin(), pile_.end()); }
 void Deck::Set()
 {
   for (int i = 0; i < 2; i++)
-  {
     pile_.push_back(Card("SPY", 0, "REF"));
-  }
   for (int i = 0; i < 6; i++)
-  {
     pile_.push_back(Card("GUARD", 1, "REF"));
-  }
   for (int i = 0; i < 2; i++)
-  {
     pile_.push_back(Card("PRIEST", 2, "REF"));
-  }
   for (int i = 0; i < 2; i++)
-  {
     pile_.push_back(Card("BARON", 3, "REF"));
-  }
   for (int i = 0; i < 2; i++)
-  {
     pile_.push_back(Card("HANDMAID", 4, "REF"));
-  }
   for (int i = 0; i < 2; i++)
-  {
     pile_.push_back(Card("PRINCE", 5, "REF"));
-  }
   for (int i = 0; i < 2; i++)
-  {
     pile_.push_back(Card("CHANCELLOR", 6, "REF"));
-  }
   pile_.push_back(Card("KING", 7, "REF"));
   pile_.push_back(Card("COUNTESS", 8, "REF"));
   pile_.push_back(Card("PRINCESS", 9, "REF"));
@@ -63,27 +49,19 @@ void Deck::Insert(Card card) { pile_.push_back(card); }
 void Deck::Print() const
 {
   if (pile_.empty())
-  {
     cout << "EMPTY" << endl;
-  }
   else
   {
     if (pile_.size() == 1)
-    {
       cout << pile_.at(0).GetName() << endl;
-    }
     else
     {
       for (size_t i = 0; i < pile_.size(); i++)
       {
         if (i < pile_.size() - 1)
-        {
           cout << pile_.at(i).GetName() << ", ";
-        }
         else
-        {
           cout << pile_.at(i).GetName() << endl;
-        }
       }
     }
   }
