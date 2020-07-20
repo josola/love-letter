6570498b7a0ead64e88bdfc3f0765aa91e95fd26
josola <jordan.sola@gmail.com>
2020-07-19 22:29:23 -0500

    Update to formatting.

--- src/card.cpp
+++ src/card.cpp
@@ -4,20 +4,40 @@
 using std::cout;
 using std::endl;
 
-Card::Card(const string name, const int value, const string description)
-    : name_(name), value_(value), description_(description) {}
+Card::Card(const string name, const int value, const string description) : name_(name), value_(value), description_(description) {}
 
-const string Card::GetName() const { return name_; }
+const string Card::GetName() const
+{
+    return name_;
+}
 
-const string Card::GetDescription() const { return description_; }
+const string Card::GetDescription() const
+{
+    return description_;
+}
 
-const int Card::GetValue() const { return value_; }
+const int Card::GetValue() const
+{
+    return value_;
+}
 
-void Card::PrintName() const { cout << name_; }
+void Card::PrintName() const
+{
+    cout << name_;
+}
 
-void Card::PrintValue() const { cout << value_; }
+void Card::PrintValue() const
+{
+    cout << value_;
+}
 
-void Card::PrintDescription() const { cout << description_; }
+void Card::PrintDescription() const
+{
+    cout << description_;
+}
 
 template <class T>
-void Action(T *obj) { cout << "CARD ACTION!" << endl; }
+void Action(T *obj)
+{
+    cout << "CARD ACTION!" << endl;
+}
