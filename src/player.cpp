7cd862912fe48d5e2b5a3ca7445bd47f06724920
josola <jordan.sola@gmail.com>
2020-07-19 22:55:44 -0500

    Update to formatting.

--- src/player.cpp
+++ src/player.cpp
@@ -12,23 +12,65 @@ using std::cout;
 using std::string;
 using std::vector;
 
-Player::Player(const string name, const int value, const Reference &reference)
-    : name_(name), value_(value), reference_(reference){};
+Player::Player(const string name, const int value, const Reference &reference) : name_(name), value_(value), reference_(reference){};
 
 // getters
-const bool Player::ProtectionStatus() const { return handmaid_; }
-const string Player::GetName() const { return name_; }
-vector<Card>* Player::GetHand() { return &hand_; }
-const bool Player::Status() const { return playing_; }
-const int Player::GetValue() const { return value_; }
-const int Player::GetTokenCount() const { return token_count_; }
-const bool Player::SpyStatus() const { return spy_; }
-Converter *Player::GetConversion() { return &convert; }
+const bool Player::ProtectionStatus() const
+{
+  return handmaid_;
+}
+
+const string Player::GetName() const
+{
+  return name_;
+}
+
+vector<Card>* Player::GetHand()
+{
+  return &hand_;
+}
+
+const bool Player::Status() const
+{
+  return playing_;
+}
+
+const int Player::GetValue() const
+{
+  return value_;
+}
+
+const int Player::GetTokenCount() const
+{
+  return token_count_;
+}
+
+const bool Player::SpyStatus() const
+{
+  return spy_;
+}
+
+Converter *Player::GetConversion()
+{
+  return &convert;
+}
 
 // setters
-void Player::Draw(const Card obj) { hand_.push_back(obj); }
-void Player::SetProtection(const bool state) { handmaid_ = state; }
-void Player::GainSpy() { spy_ = true; }
+void Player::Draw(const Card obj)
+{
+  hand_.push_back(obj);
+}
+
+void Player::SetProtection(const bool state)
+{
+  handmaid_ = state;
+}
+
+void Player::GainSpy()
+{
+  spy_ = true;
+}
+
 void Player::Reset()
 {
   hand_.clear();
@@ -37,6 +79,7 @@ void Player::Reset()
   winner_ = false;
   playing_ = true;
 }
+
 void Player::Discard(const int choice, Deck &deck)
 {
   for (Card &iCard : hand_)
@@ -49,6 +92,7 @@ void Player::Discard(const int choice, Deck &deck)
     }
   }
 }
+
 void Player::DiscardHand(Deck &deck)
 {
   for (Card &iCard : hand_)
@@ -56,8 +100,17 @@ void Player::DiscardHand(Deck &deck)
     Discard(iCard.GetValue(), deck);
   }
 }
-void Player::Addtoken() { token_count_++; }
-void Player::Winner(const bool state) { winner_ = state; }
+
+void Player::Addtoken()
+{
+  token_count_++;
+}
+
+void Player::Winner(const bool state)
+{
+  winner_ = state;
+}
+
 void Player::RemoveCard(const int card)
 {
   for (size_t i = 0; i < hand_.size(); i++)
@@ -69,6 +122,7 @@ void Player::RemoveCard(const int card)
     }
   }
 }
+
 void Player::Out(Deck &deck)
 {
   this->DiscardHand(deck);
@@ -79,8 +133,8 @@ void Player::Out(Deck &deck)
 // printers
 void Player::PrintHand() const
 {
-  cout << '\n'
-       << this->GetName() << " hand:\n";
+  cout << '\n' << this->GetName() << " hand:\n";
+  
   if (!hand_.empty())
   {
     for (size_t i = 0; i < hand_.size(); i++)
