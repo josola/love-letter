f0aa5530cb82c6328b9e4bf1ff919fa3dc7e0622
josola <jordan.sola@gmail.com>
2020-07-23 21:27:01 -0500

    remove duplicate function declarations
    Removed function declarations that were moved to the "Input"
    source files.

--- src/card_actions.h
+++ src/card_actions.h
@@ -22,18 +22,6 @@ class Player;
 class Deck;
 class Card;
 
-// supporting functions
-const bool CheckCard(const int card);
-void FixCard(int card);
-void SanitizeCard(int target, const int originator);
-const bool CheckTarget(const int input, const GameState &state);
-void FixTarget(int input, const GameState &state);
-void SanitizeTarget(int target, const GameState &state, const int card, Player &aggressor);
-Player *GetTarget(Player &aggressor, GameState &state, const int card);
-const bool CheckCharacter(const char input, const char target);
-void FixCharacter(char input, const char target);
-void SanitizeCharacter(char input, const char target);
-
 // actions
 void Spy(Player &player);
 void Guard(GameState &state, Player &aggressor, vector<Card> &deck);
