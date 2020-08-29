cd2572dee8982942182b4fa2cdde75128719b212
josola <jordan.sola@gmail.com>
2020-08-29 08:06:14 -0500

    initial source commit
    Initial source commit for an opponent player. Small update to ai banner.

---
+++ src/opponent.h
@@ -0,0 +1,10 @@
+//===-- opponent.h - determines opponent capabilities -------*- C++ -*-===//
+//
+// This software uses the MIT license.
+// See https://mit-license.org/ for license information.
+//
+//===----------------------------------------------------------------------===//
+///
+/// This file contains the definitions for an opponent.
+///
+//===----------------------------------------------------------------------===//
