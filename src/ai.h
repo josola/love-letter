908ed98c395b75f1754f6c48521294c40cdfd978
josola <jordan.sola@gmail.com>
2020-08-29 07:56:32 -0500

    initial source commit
    Initial source file commit.

---
+++ src/ai.h
@@ -0,0 +1,10 @@
+//===-- ai.h - determines ai capabilities -------*- C++ -*-===//
+//
+// This software uses the MIT license.
+// See https://mit-license.org/ for license information.
+//
+//===----------------------------------------------------------------------===//
+///
+/// This file contains the definitions for an AI opponent.
+///
+//===----------------------------------------------------------------------===//
